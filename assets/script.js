e1b6ce2fdd487d4d0cbc2b7c6c6c6456f4a1c94d
AntiqueRised <imjoodie@gmail.com>
2023-04-10 18:34:48 -0500

    MVP, all functionality except recent buttons/ a few bugs

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
+$("#info-container").hide()
+$("#forecast-container").hide()
+
+cityNamesArray = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
+ 
+$("#userInput").autocomplete({
+  source: function(request, response) {
+      var results = $.ui.autocomplete.filter(cityNamesArray, request.term);
+      response(results.slice(0, 10));
+  }
+});
+
+let recentSearchesArray = JSON.parse(localStorage.getItem("recentCitiesTable"));
+if (recentSearchesArray === null) {
+  recentSearchesArray = [];
+}
 
 //var userSearch = document.getElementById("userInput");
 var day1 = [$('#date1'), $('#temp1'), $('#wind1'), $('#humidity1')];
@@ -15,20 +31,27 @@ var day5Img = $("#img5");
 var day6Img = $("#img6");
 
 var searchBtn = $("#searchBtn");
-//var userSearch = $("#userInput").val.trim;
-var userSearch = "austin "
+var search = $("#userInput")
 
-console.log(userSearch);
+//var userSearch = $("#userInput").val.trim;
+
+searchBtn.click(function () {
+  $("#info-container").show()
+  $("#forecast-container").show()
+
+  var search = $("#userInput").val() + " ";
+  console.log(search);
+
+  var userSearch = search
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&appid=561ba66c8fb7676c4035b87bda0066fb&units=imperial`;
+
+  recentSearchesArray.push({city:$("#userInput").val()});
+  localStorage.setItem("recentCitiesTable", JSON.stringify(recentSearchesArray));
+  console.log("LocalStorage")
+  searchHistory()
 
 //5 DAY FORECAST API CALL
-var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&appid=561ba66c8fb7676c4035b87bda0066fb&units=imperial`;
-
-// GEOCODE API CALL
-//var geocode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
-
-
-
-fetch(requestUrl)
+    fetch(requestUrl)
   .then(response => response.json())
   .then(function (data) {
 
@@ -47,7 +70,7 @@ fetch(requestUrl)
     console.log(humidity);
 
     //inputs fetch request info into DOM 
-    day1[0].text(userSearch.toUpperCase() + (data.list[0].dt_txt.slice(0, -8)));
+    day1[0].text(userSearch.charAt(0).toUpperCase() + userSearch.slice(1) + (data.list[0].dt_txt.slice(0, -8)));
     day1[1].text("Temp: " + (data.list[0].main.temp) + "ºF");
     day1[2].text("Wind: " + (data.list[0].wind.speed) + " mph");
     day1[3].text("Humidity: " + (data.list[0].main.humidity) + "%");
@@ -89,18 +112,35 @@ fetch(requestUrl)
     var day6Icon = data.list[37].weather[0].icon;
     day6Img[0].src = `https://openweathermap.org/img/w/${day6Icon}.png`;
   });
+})
+
+function searchHistory() {
+  var recentSearchesArray = JSON.parse(localStorage.getItem("recentCitiesTable"));
+  $(".btn-container").empty();
+
+  for (i = 0; i < recentSearchesArray.length; i++) {
+    var appendBlock = `<button type="button" class="btn btn-secondary my-2 col-12"> ${recentSearchesArray[i].city} </button>`;
+    $(".btn-container").append(appendBlock);
+  }
+};
+searchHistory()
 
 
-function searchItem () {
-    $("#searchBtn").on("click", function (event) {
 
-        event.preventDefault();
-    
-        var userSearch = $("#userInput").val().trim();
-    
-        if (city === "") {
-            return;
-        };
-    });
-}
-  
+
+
+
+
+
+
+
+
+
+
+
+
+// GEOCODE API CALL
+//var geocode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+
+
+
